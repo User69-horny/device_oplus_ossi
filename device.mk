b52d6adadd275c917164663ab6b93e5b30842090
User69-horny <yesntyesnt786+1@gmail.com>
2024-09-26 13:48:48 +0530

    Downgrade device.mk

--- device.mk
+++ device.mk
@@ -5,37 +5,38 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/oplus/ossi
+LOCAL_PATH := device/oplus/vitamin
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Enable developer GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # A/B
-AB_OTA_UPDATER := true
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    system_ext \
-    product \
-    vendor \
-    vendor_boot \
-    odm \
-    vbmeta \
-    vbmeta_system 
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
+# Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6983
+
+#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6983 \
-    bootctrl.mt6983.recovery
+    libgptutils \
+    libz \
+    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -44,31 +45,12 @@ PRODUCT_PACKAGES += \
     update_verifier \
     update_engine_sideload
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
-
-# Recovery libs
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion 
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-# Mtk plpath utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
-
-# Soong
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Shipping level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
